3eafe8de1540d143eb7605cff71ef466d88c5c5b
Sachin R S <sachinsrs7@gmail.com>
2021-07-17 11:39:40 +0530

    deleted duplicate

--- CowsAndBulls/feedback/style.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(() => {
-    $('#title').focus();
-    // $('#text').autosize();
-    autosize($('#text'));
-    window.autosize = window.autosize ? window.autosize : require('autosize');
-});
